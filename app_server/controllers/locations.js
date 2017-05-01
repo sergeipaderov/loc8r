6f3f6ac4a40729bcabbe9cbe7d890811c47c15d1
Sergei Paderov <sergeipaderov@gmail.com>
2017-05-01 13:52:15 -0300

    commit

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,7 +1,32 @@
 /* Get home page */
 
 module.exports.homelist = function(req, res) {
-    res.render('locations-list', {title: 'Home'});
+    res.render('locations-list', {
+        title: 'Loc8r - Find a place to work with WiFi',
+        pageHeader: {
+            title: 'Loc8r',
+            strapline: 'Find places to work with WiFi near you!'
+        },
+        locations: [{
+            name: 'Starcups',
+            adress: '125 High Street, Reading, RG6 1PS',
+            rating: 3,
+            facilities: ['Hot drinks', 'Food', 'Premium WiFi'],
+            distance: '100m'
+        }, {
+            name: 'Cafe Hero',
+            adress: '125 High Street, Reading, RG6 1PS',
+            rating: 4,
+            facilities: ['Hot drinks', 'Food', 'Premium WiFi'],
+            distance: '200m'
+        }, {
+            name: 'Burger Queen',
+            adress: '125 High Street, Reading, RG6 1PS',
+            rating: 2,
+            facilities: ['Food', 'Premium WiFi'],
+            distance: '250m'
+        }]
+    });
 };
 
 /* Get Location info */
