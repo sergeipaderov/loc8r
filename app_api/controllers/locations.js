d59aa0671702ad4bfaa04b554c8710b4d01bfd32
Sergei Paderov <sergeipaderov@gmail.com>
2017-05-15 14:12:29 -0300

    controllers

--- app_api/controllers/locations.js
+++ app_api/controllers/locations.js
@@ -1,17 +1,91 @@
 var mongoose = require('mongoose');
 var Loc = mongoose.model('Location');
 
+var theEarth = (function() {
+    var earthRadius = 6371;
+
+    var getDistanceFromRads = function(rads) {
+        return parseFloat(rads * earthRadius);
+    };
+
+    var getRadsFromDistance = function(distance) {
+        return parseFloat(distance / earthRadius);
+    };
+
+    return {
+        getDistanceFromRads : getDistanceFromRads,
+        getRadsFromDistance : getRadsFromDistance
+    };
+})();
+
 var sendJsonResponse = function(res, status, content) {
     res.status(status);
     res.json(content);
 };
 
 module.exports.locationsListByDistance = function (req, res) {
-    sendJsonResponse(res, 200, {"status" : "success"});
+    var lng = parseFloat(req.query.lng);
+    var lat = parseFloat(req.query.lat);
+    var point = {
+        type: "Point",
+        coordinates: [lng, lat]
+    };
+    var geoOptions = {
+        spherical: true,
+        maxDistance: theEarth.getRadsFromDistance(20),
+        num: 10
+    };
+    if (!lng || !lat) {
+        sendJsonResponse(res, 404, {
+            "message": "lng and lat query parameters are required"
+        });
+        return;
+    }
+    Loc.geoNear(point, geoOptions, function(err, results, stats){
+        var locations = [];
+        if (err) {
+            sendJsonResponse(res, 404, err);
+        } else {
+            results.forEach(function(doc) {
+                locations.push({
+                    distance: theEarth.getDistanceFromRads(doc.dis),
+                    name: doc.obj.address,
+                    address: doc.obj.address,
+                    rating: doc.obj.rating,
+                    facilities: doc.obj.facilities,
+                    _id: doc.obj._id
+                });
+            });
+            sendJsonResponse(res, 200, locations);
+        }
+    });
 };
 
 module.exports.locationsCreate = function (req, res) {
-    sendJsonResponse(res, 200, {"status" : "success"});
+    Loc.create({
+        name: req.body.address,
+        address: req.body.address,
+        facilities: req.body.facilities.split(","),
+        coords: [parseFloat(req.body.lng),
+                parseFloat(req.body.lat)],
+        openingTimes: [{
+            days: req.body.days1,
+            opening: req.body.opening1,
+            closing: req.body.closing1,
+            closed: req.body.closed1,
+        }, {
+            days: req.body.days2,
+            opening: req.body.opening2,
+            closing: req.body.closing2,
+            closed: req.body.closed2,
+        }]
+    }, function(err, location) {
+        if (err) {
+            sendJsonResponse(res, 400, err);
+        } else {
+            sendJsonResponse(res, 201, location);
+        }
+    });
 };
 
 module.exports.locationsReadOne = function (req, res) {
@@ -41,7 +115,13 @@ module.exports.locationsReadOne = function (req, res) {
 
 
 module.exports.locationsUpdateOne = function (req, res) {
-    sendJsonResponse(res, 200, {"status" : "success"});
+    if (!req.params.locationid) {
+        sendJsonResponse(res, 404, {
+            "message": "Not found, locationid is required"
+        });
+        return;
+    }
+    
 };
 
 module.exports.locationsDeleteOne = function (req, res) {
